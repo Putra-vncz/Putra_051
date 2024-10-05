5e458ddff212fef1e861ad060c74a3a718b2c9b6
Putra-vncz <183426459+Putra-vncz@users.noreply.github.com>
2024-10-05 20:33:12 +0900

    Create Kasir.py

---
+++ Kasir.py
@@ -0,0 +1,24 @@
+print("Kasir Perusahaan Boneka\n")
+
+boneka = int(input("Masukkan Jumlah Pembelian Boneka\t: "))
+
+if boneka < 1:
+    print("\nError\n")
+elif boneka <= 12:
+    print("\nHarga Perboneka Rp20.000\n")
+    harga = 20000
+elif boneka <= 24:
+    print("\nHarga Perboneka Rp19.500\n")
+    harga = 19500
+elif boneka <= 50:
+    print("\nHarga Perboneka Rp18.000\n")
+    harga = 18000
+elif boneka > 50:
+    print("\nHarga Perboneka Rp17.000\n")
+    harga = 17000
+else:
+    print("\nError\n")
+
+bayar = boneka * harga
+
+print("Total Bayar = Rp",bayar)
